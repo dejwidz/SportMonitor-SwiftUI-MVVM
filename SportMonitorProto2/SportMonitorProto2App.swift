48d26843d632cf59d9d1a90ede1f0d70a3b64fb8
Dawid Zimoch <dawid.jerzy.zimoch@gmail.com>
2023-09-10 18:04:16 +0200

    Initial Commit

---
+++ SportMonitorProto2/SportMonitorProto2App.swift
@@ -0,0 +1,17 @@
+//
+//  SportMonitorProto2App.swift
+//  SportMonitorProto2
+//
+//  Created by Dawid Zimoch on 10/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct SportMonitorProto2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
